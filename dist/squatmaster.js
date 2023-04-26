435a6715042ca4fd06320f9843861615b7ae8aba
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-04-26 13:31:41 +0100

    Latest built code

--- dist/squatmaster.js
+++ dist/squatmaster.js
@@ -371,13 +371,255 @@ AFRAME.registerComponent('bar-position', {
 
 /***/ }),
 
+/***/ "./src/calibration-flow.js":
+/*!*********************************!*\
+  !*** ./src/calibration-flow.js ***!
+  \*********************************/
+/***/ (() => {
+
+const calibrationUIMessages = {
+  start: 'Ensure your safety pins are in place at a suitable height.  Position the hooks just below shoulder height, and place an empty bar across the hooks.',
+  hooks: 'Without moving the bar yet, duck under and brace your shoulders against the bar',
+  top: 'Stand up to your full height, and take a step back',
+  depth: 'Squat down to your target depth: thighs parallel to the floor',
+  safety: 'Bend down further, so that the bar sits on your safety pins',
+  review: 'Stand up again.  If you are happy, nod, and return the bar to the rack',
+  done: 'Calibration is now complete'
+}
+
+const calibrationUIPositions = {
+  start: '',
+  hooks: 'racked bar position',
+  top: 'height at top of squat',
+  depth: 'target depth',
+  safety: 'safety pin depth',
+  review: '',
+  done: '',
+}
+
+AFRAME.registerComponent('calibration-flow', {
+
+  dependencies: ['bar-position'],
+
+  init() {
+
+    this.nodEvent = this.nodEvent.bind(this)
+    this.shakeEvent = this.shakeEvent.bind(this)
+    
+    this.el.addEventListener('nod', this.nodEvent);
+    this.el.addEventListener('shake', this.shakeEvent);
+
+    // UI state
+    this.stage = 'start' // one of: start, hooks, top, depth, safety, review, done
+    this.saving = false
+    this.deleting = false
+
+    this.calibrationUI = document.getElementById('calibration-ui')
+  },
+
+  update() {
+    this.updateUI()
+  },
+
+  remove() {
+    this.el.removeEventListener('nod', this.nodEvent);
+    this.el.removeEventListener('shake', this.shakeEvent);
+  },
+
+  beginCalibrationProcess() {
+
+    this.stage = 'start'
+    this.calibrationUI.object3D.visible = true
+    this.updateUI()
+
+  },
+
+  endCalibrationProcess() {
+    this.calibrationUI.object3D.visible = false
+    this.el.emit('calibration-complete')
+  },
+
+  getBarPosition() {
+
+    const component = this.el.components['bar-position']
+    return component.barPosition
+  },
+
+  // displays "saving" message for 1 second, then moves to next stage.
+  moveToStage(stage) {
+
+    this.saving = true
+    this.updateUI()
+
+    setTimeout(() => {
+      this.stage = stage
+      this.saving = false
+      this.updateUI()
+    }, 1000)
+  },
+
+  // equivalent function when moving back a stage...
+  moveBackToStage(stage) {
+
+    this.deleting = true
+
+    // short timeout, just to avoid duplicate shakes...
+    setTimeout(() => {
+      this.stage = stage
+      this.deleting = false
+      this.updateUI()
+    }, 500)
+  },
+
+  nodEvent() {
+
+    let yPos
+
+    switch (this.stage) {
+
+      case 'start':
+        // no data to record yet.
+        this.moveToStage('hooks')
+        break
+
+      case 'hooks':
+        // new Vector3 to ensure update happens.
+        const pos = new THREE.Vector3()
+        pos.copy(this.getBarPosition())
+        this.el.setAttribute('bar-monitor', {hookPosition: pos})
+        this.moveToStage('top')
+        break
+  
+      case 'top':
+        ypos = this.getBarPosition().y
+        this.el.setAttribute('bar-monitor', {topHeight: ypos})
+        this.moveToStage('depth')
+        break
+
+      case 'depth':
+        ypos = this.getBarPosition().y
+        this.el.setAttribute('bar-monitor', {targetDepth: ypos})
+        this.moveToStage('safety')
+        break
+
+      case 'safety':
+        ypos = this.getBarPosition().y
+        this.el.setAttribute('bar-monitor', {safetyPinHeight: ypos})
+        this.moveToStage('review')
+        break
+
+      case 'review':
+        this.moveToStage('done')
+        break
+
+      case 'done':
+        this.endCalibrationProcess()
+        break
+
+      default:
+        console.error("Hit Nod in unexpected state: ", this.stage)
+        break
+    }
+  },
+
+  shakeEvent() {
+
+    switch (this.stage) {
+
+      case 'start':
+        // no effect
+        break
+
+      case 'hooks':
+        this.moveBackToStage('start')
+        break
+  
+      case 'top':
+        this.moveBackToStage('hooks')
+        break
+
+      case 'depth':
+        this.moveBackToStage('top')
+        break
+
+      case 'safety':
+        this.moveBackToStage('depth')
+        break
+
+      case 'review':
+        this.moveBackToStage('safety')
+        break
+
+      case 'done':
+        // no effect - too late
+        // (TBC - we could desig a flow to restart the whole calibration process again)
+        break
+         
+      default:
+        console.error("Hit Nod in unexpected state: ", this.stage)
+        break
+    }
+  },
+
+  updateUI() {
+
+    this.calibrationUI.setAttribute('calibration-ui',
+                                     {saving: this.saving,
+                                      deleting: this.deleting,
+                                      position: calibrationUIPositions[this.stage],
+                                      message: calibrationUIMessages[this.stage]})
+  }
+})
+
+AFRAME.registerComponent('calibration-ui', {
+
+  schema: {
+    saving: {default: false},
+    deleting: {default: false},
+    position: {default: ''},
+    message: {default: ''},
+  },
+
+  init() {
+    this.circle = document.createElement('a-circle')
+    this.circle.setAttribute('radius', 1)
+    this.circle.setAttribute('color', 'yellow')
+    this.el.appendChild(this.circle)
+
+    this.text = document.createElement('a-entity')
+    this.text.setAttribute('text', {color: 'black',
+                                    width: 2,
+                                    wrapCount: 40,
+                                    align: 'center'})
+    this.circle.appendChild(this.text)
+  },
+
+  update() {
+
+    let string = ''
+    if (this.data.saving) {
+      string += `Saving ${this.data.position}...\n\n`
+    }
+    if (this.data.deleting) {
+      string += `Deleting ${this.data.position}...\n\n`
+    }
+
+    string += this.data.message
+
+    this.text.setAttribute('text', `value: ${string}`)
+  }
+});
+
+
+/***/ }),
+
 /***/ "./src/in-rack-ui.js":
 /*!***************************!*\
   !*** ./src/in-rack-ui.js ***!
   \***************************/
 /***/ (() => {
 
-AFRAME.registerComponent('in-rack-ui', {
+AFRAME.registerComponent('inside-rack-ui', {
 
   schema: {
     repsToGo: {default: 5},
@@ -390,7 +632,7 @@ AFRAME.registerComponent('in-rack-ui', {
     this.el.appendChild(this.circle)
 
     this.text = document.createElement('a-entity')
-    this.text.setAttribute('text', {color:  'black',
+    this.text.setAttribute('text', {color: 'black',
                                     wrapCount: 15,
                                     align: 'center'})
     this.circle.appendChild(this.text)
@@ -705,6 +947,82 @@ AFRAME.registerComponent('squat-rack', {
 
 /***/ }),
 
+/***/ "./src/ui-manager.js":
+/*!***************************!*\
+  !*** ./src/ui-manager.js ***!
+  \***************************/
+/***/ (() => {
+
+// Top-level UI component that manages visibility of other UIs
+AFRAME.registerComponent('ui-manager', {
+
+  init() {
+    this.state = {
+      calibrated: false,
+      inRack: false,
+    }
+
+    this.calibrationComplete = this.calibrationComplete.bind(this)
+    this.enteredRack = this.enteredRack.bind(this)
+    this.exitedRack = this.exitedRack.bind(this)
+    this.el.addEventListener('calibration-complete', this.calibrationComplete)
+    this.el.addEventListener('entered-rack', this.enteredRack);
+    this.el.addEventListener('exited-rack', this.exitedRack);
+
+
+    this.calibrationUI = document.getElementById('calibration-ui')
+    this.insideRackUI = document.getElementById('inside-rack-ui')
+    this.outsideRackUI = document.getElementById('outside-rack-ui')
+
+    this.updateUIs()
+  },
+
+  calibrationComplete() {
+    this.state.calibrated = true
+    this.updateUIs()
+  },
+
+  enteredRack() {
+    this.state.inRack = true
+    this.updateUIs()
+  },
+
+  exitedRack() {
+    this.state.inRack = false
+    this.updateUIs()
+  },
+
+  updateUIs() {
+
+    updateVisibility = (el, visible) => {
+      if (el && el.object3D) {
+        el.object3D.visible = visible
+      }
+    }
+
+    if (this.state.calibrated) {
+
+      if (this.state.inRack) {
+        updateVisibility(this.calibrationUI, false)
+        updateVisibility(this.insideRackUI, true)
+        updateVisibility(this.outsideRackUI, false)
+      }
+      else {
+        updateVisibility(this.calibrationUI, false)
+        updateVisibility(this.insideRackUI, false)
+        updateVisibility(this.outsideRackUI, true)
+      }
+    }
+    else {
+      updateVisibility(this.calibrationUI, true)
+      updateVisibility(this.insideRackUI, false)
+      updateVisibility(this.outsideRackUI, false)
+    }
+  }
+})
+
+/***/ }),
+
 /***/ "./src/ui-updater.js":
 /*!***************************!*\
   !*** ./src/ui-updater.js ***!
@@ -719,8 +1037,6 @@ AFRAME.registerComponent('ui-updater', {
 
   init() {
 
-    this.enteredRack = this.enteredRack.bind(this)
-    this.exitedRack = this.exitedRack.bind(this)
     this.reachedHooks = this.reachedHooks.bind(this)
     this.leftHooks = this.leftHooks.bind(this)
     this.shoulderedBar = this.shoulderedBar.bind(this)
@@ -732,8 +1048,6 @@ AFRAME.registerComponent('ui-updater', {
     this.belowSafetyPins = this.belowSafetyPins.bind(this)
     this.bailedOut = this.bailedOut.bind(this)
 
-    this.el.addEventListener('entered-rack', this.enteredRack);
-    this.el.addEventListener('exited-rack', this.exitedRack);
     this.el.addEventListener('reached-hooks', this.reachedHooks);
     this.el.addEventListener('left-hooks', this.leftHooks);
     this.el.addEventListener('shouldered-bar', this.shoulderedBar);
@@ -756,8 +1070,6 @@ AFRAME.registerComponent('ui-updater', {
   },
 
   remove() {
-    this.el.removeEventListener('entered-rack', this.enteredRack);
-    this.el.removeEventListener('exited-rack', this.exitedRack);
     this.el.removeEventListener('reached-hooks', this.reachedHooks);
     this.el.removeEventListener('left-hooks', this.leftHooks);
     this.el.removeEventListener('shouldered-bar', this.shoulderedBar);
@@ -773,38 +1085,13 @@ AFRAME.registerComponent('ui-updater', {
   setTargetReps(repCount) {
 
     this.state.repsToGo = repCount
-    this.insideRackUI.setAttribute('in-rack-ui', {repsToGo: this.state.repsToGo})
+    this.insideRackUI.setAttribute('inside-rack-ui', {repsToGo: this.state.repsToGo})
   },
 
   repCompleted() {
 
     this.state.repsToGo--
-    this.insideRackUI.setAttribute('in-rack-ui', {repsToGo: this.state.repsToGo})
-  },
-
-  enteredRack() {
-
-    if (this.insideRackUI) {
-      this.insideRackUI.setAttribute('visible', true)
-    }
-    
-    if (this.outsideRackUI) {
-      this.outsideRackUI.setAttribute('visible', false)
-    }
-
-    this.setTargetReps(5)
-
-  },
-
-  exitedRack() {
-
-    if (this.insideRackUI) {
-      this.insideRackUI.setAttribute('visible', false)
-    }
-    
-    if (this.outsideRackUI) {
-      this.outsideRackUI.setAttribute('visible', true)
-    }
+    this.insideRackUI.setAttribute('inside-rack-ui', {repsToGo: this.state.repsToGo})
   },
 
   reachedHooks() {
@@ -825,7 +1112,7 @@ AFRAME.registerComponent('ui-updater', {
   },
 
   setMessage(message) {
-    this.insideRackUI.setAttribute('in-rack-ui', {message: message})
+    this.insideRackUI.setAttribute('inside-rack-ui', {message: message})
   },
 
   hitTop() {
@@ -1025,6 +1312,8 @@ __webpack_require__(/*! ./src/ui-updater.js */ "./src/ui-updater.js")
 __webpack_require__(/*! ./src/squat-rack.js */ "./src/squat-rack.js")
 __webpack_require__(/*! ./src/vertical-controls.js */ "./src/vertical-controls.js")
 __webpack_require__(/*! ./src/nod-shake.js */ "./src/nod-shake.js")
+__webpack_require__(/*! ./src/calibration-flow */ "./src/calibration-flow.js")
+__webpack_require__(/*! ./src/ui-manager */ "./src/ui-manager.js")
 })();
 
 /******/ 	return __webpack_exports__;
