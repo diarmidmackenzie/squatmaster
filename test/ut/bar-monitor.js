bdd6312d50fabb5d952492918e4d6ae822be9709
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-04-26 18:36:43 +0100

    Initial attempt to fix some UTs

--- test/ut/bar-monitor.js
+++ test/ut/bar-monitor.js
@@ -3,7 +3,12 @@ QUnit.module('basic tests', {
     const scene = createScene();
     scene.setAttribute('bar-position', '');
     scene.setAttribute('bar-monitor', '');
-    setCameraPosition(scene, 0, 1.6, 1);
+    setCameraPosition(scene, 0, 1.6, -1);
+
+    scene.insertAdjacentHTML('afterBegin', 
+    `<a-entity id="rack" squat-rack position="0 0 -1"></a-entity>`
+    )
+
   },
   afterEach: function() {
     const scene = document.querySelector('a-scene');
@@ -16,10 +21,13 @@ QUnit.test('lifter starts inside rack - received entered-rack event', function(a
   const scene = document.querySelector('a-scene')
   const done = assert.async();
 
-  scene.addEventListener('entered-rack', () => {
-    done()
-    assert.expect(0)
-  }, {once: true})
+  scene.addEventListener('loaded', () => {
+
+    scene.addEventListener('entered-rack', () => {
+      done()
+      assert.expect(0)
+    }, {once: true})
+  })
 
 })
 
@@ -28,16 +36,18 @@ QUnit.test('lifter moves out of rack - received exited-rack event', function(ass
   const scene = document.querySelector('a-scene')
   const done = assert.async();
 
-  // wait for entered-rack event before moving out of rack, or we won't get an exited rack event.
-  scene.addEventListener('entered-rack', () => {
-    scene.addEventListener('exited-rack', () => {
-      done()
-      assert.expect(0)
+  scene.addEventListener('loaded', () => {
+
+    // wait for entered-rack event before moving out of rack, or we won't get an exited rack event.
+    scene.addEventListener('entered-rack', () => {
+      scene.addEventListener('exited-rack', () => {
+        done()
+        assert.expect(0)
+      }, {once: true})
+
+      scene.camera.position.set(0, 1.6, 2)
     }, {once: true})
-
-    scene.camera.position.set(0, 1.6, -2)
-  }, {once: true})
-
+  })
 })
 
 QUnit.test('full sequence: lifter lifts bar, squats and puts bar back onto hooks', function(assert) {
@@ -45,43 +55,44 @@ QUnit.test('full sequence: lifter lifts bar, squats and puts bar back onto hooks
   const scene = document.querySelector('a-scene')
   const done = assert.async();
 
-  setCameraPosition(scene, 0, 1.6, 1) 
+  scene.addEventListener('loaded', () => {
 
-  // enter rack, then move to reach hooks.
-  scene.addEventListener('entered-rack', () => {
+    // enter rack, then move to reach hooks.
+    scene.addEventListener('entered-rack', () => {
 
-    scene.camera.parent.position.set(0, 1.6, 1.7)
-    assert.false(scene.components['bar-monitor'].state.barOnShoulders) 
+      scene.camera.parent.position.set(0, 1.6, -1.7)
+      assert.false(scene.components['bar-monitor'].state.barOnShoulders) 
 
-    scene.addEventListener('reached-hooks', () => {
+      scene.addEventListener('reached-hooks', () => {
 
-      scene.camera.parent.position.set(0, 1.7, 1.6)
+        scene.camera.parent.position.set(0, 1.7, -1.6)
 
-      scene.addEventListener('left-hooks', () => {
-
-        assert.true(scene.components['bar-monitor'].state.barOnShoulders) 
-        scene.camera.parent.position.set(0, 1.8, 1)
-
-        scene.addEventListener('hit-top', () => {
+        scene.addEventListener('left-hooks', () => {
 
           assert.true(scene.components['bar-monitor'].state.barOnShoulders) 
-          scene.camera.parent.position.set(0, 0.59, 1)
+          scene.camera.parent.position.set(0, 1.8, -1)
 
-          scene.addEventListener('hit-target-depth', () => {
+          scene.addEventListener('hit-top', () => {
 
-            scene.camera.parent.position.set(0, 1.8, 1)
-            scene.addEventListener('hit-top', () => {
+            assert.true(scene.components['bar-monitor'].state.barOnShoulders) 
+            scene.camera.parent.position.set(0, 0.59, -1)
 
-              assert.true(scene.components['bar-monitor'].state.barOnShoulders) 
-              scene.camera.parent.position.set(0, 1.6, 1.7)
+            scene.addEventListener('hit-target-depth', () => {
 
-              scene.addEventListener('reached-hooks', () => {
+              scene.camera.parent.position.set(0, 1.8, -1)
+              scene.addEventListener('hit-top', () => {
+
                 assert.true(scene.components['bar-monitor'].state.barOnShoulders) 
-                scene.camera.parent.position.set(0, 1.4, 1.5)
+                scene.camera.parent.position.set(0, 1.6, -1.7)
 
-                scene.addEventListener('left-hooks', () => {
-                  assert.false(scene.components['bar-monitor'].state.barOnShoulders)
-                  done()
+                scene.addEventListener('reached-hooks', () => {
+                  assert.true(scene.components['bar-monitor'].state.barOnShoulders) 
+                  scene.camera.parent.position.set(0, 1.4, -1.5)
+
+                  scene.addEventListener('left-hooks', () => {
+                    assert.false(scene.components['bar-monitor'].state.barOnShoulders)
+                    done()
+                  }, {once: true})
                 }, {once: true})
               }, {once: true})
             }, {once: true})
@@ -89,6 +100,6 @@ QUnit.test('full sequence: lifter lifts bar, squats and puts bar back onto hooks
         }, {once: true})
       }, {once: true})
     }, {once: true})
-  }, {once: true})
+  })
 })
 
